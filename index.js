87d2caa4cc101f38491c97d255ac052d1a5d017e
Aleksey <aleksey.o@it-dimension.com>
2023-07-15 22:47:33 +0300

    init .

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/',(req, res) =>  {
+
+   let hostName_header = req.header("host"); //dotnetpattern.com
+
+    res.send(hostName_header);
+});
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
