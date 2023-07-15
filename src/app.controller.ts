57deeece54bc9152215ba9d6ee1873d8dff596dd
Aleksey <aleksey.o@it-dimension.com>
2023-07-15 23:09:10 +0300

    wdwd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Req} from '@nestjs/common';
 import { AppService } from './app.service';
-
+import {Request} from 'express';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +8,14 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("url")
+  getUrl(@Req() req: Request): string {
+    const protocol = req.protocol;
+    const host = req.get("Host");
+    const originUrl = req.originalUrl;
+    const fullUrl = protocol + host + originUrl;
+   return fullUrl
   }
 }
