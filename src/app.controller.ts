e56c00cfb0017583801d1cfffe914ce84a74f4ce
Aleksey <aleksey.o@it-dimension.com>
2023-07-30 21:27:45 +0300

    yyyy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,6 @@
 import {Controller, Get, Req, Ip} from '@nestjs/common';
 import { AppService } from './app.service';
 import {Request} from 'express';
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
@@ -25,7 +24,7 @@ export class AppController {
     // console.log('IP', req.ip)
     // console.log('IP2', ip)
     // console.log('fullUrl', protocol + host + originUrl)
-    return {"ROOM":{"ID":"1C4021","BedName":"1C-402-1","PatientName":"Forharrisoneonly Donotuse","attendingphys":"WPH-Physician Generic","attendingphysprefix":"Dr.","attendingphysdegree":"MD","primaryRN":"integretED Primary RN","secondRN":"integretED Secondary RN","event1Time":"202307251211","e1-appointmentName":"X-Ray Clinic","event2Time":"202307252145","e2-appointmentName":"Rehab Plus","event3Time":"202307252245","e3-appointmentName":"MSK Clinic", "event4Time":"202307302300","e4-appointmentName":"Howdy"}}
+    return {"ROOM":{"ID":"1C4021","BedName":"1C-402-1","PatientName":"Forharrisoneonly Donotuse","attendingphys":"WPH-Physician Generic","attendingphysprefix":"Dr.","attendingphysdegree":"MD","primaryRN":"integretED Primary RN","secondRN":"integretED Secondary RN","event1Time":"202307301311","e1-appointmentName":"X-Ray Clinic","event2Time":"202307302245","e2-appointmentName":"Rehab Plus","event3Time":"202307302345","e3-appointmentName":"MSK Clinic", "event4Time":"202307302350","e4-appointmentName":"Howdy"}}
 
   }
 }
