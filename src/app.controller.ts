3b106c7bb7330d1c5bdb9e1ae315aed5ba06ddca
Aleksey <aleksey.o@it-dimension.com>
2023-07-31 11:25:10 +0300

    image

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,16 +15,18 @@ export class AppController {
   getUrl(@Req() req: Request, @Ip() ip): any {
     console.log(req.headers)
     console.log(req.headers[''])
-    // const host = req.get("Host");
-    // const ipAddress = req.header('x-forwarded-for') ||
-    //     req.socket.remoteAddress;
-    // console.log('ipAddress', ipAddress)
-    // const originUrl = req.originalUrl;
-    // const fullUrl = protocol + host + originUrl;
-    // console.log('IP', req.ip)
-    // console.log('IP2', ip)
-    // console.log('fullUrl', protocol + host + originUrl)
     return {"ROOM":{"ID":"1C4021","BedName":"1C-402-1","PatientName":"Forharrisoneonly Donotuse","attendingphys":"WPH-Physician Generic","attendingphysprefix":"Dr.","attendingphysdegree":"MD","primaryRN":"integretED Primary RN","secondRN":"integretED Secondary RN","event1Time":"202307302115","e1-appointmentName":"X-Ray Clinic","event2Time":"202307302116","e2-appointmentName":"Rehab Plus","event3Time":"202307302345","e3-appointmentName":"MSK Clinic", "event4Time":"202307302350","e4-appointmentName":"Howdy"}}
 
   }
+
+  @Get("images")
+  getImages(): any {
+    return [
+        'https://iili.io/HZMZfZN.md.png',
+        'https://iili.io/HZMZu9f.md.png',
+        'https://iili.io/HZMZI8G.md.png'
+    ]
+
+  }
+
 }
